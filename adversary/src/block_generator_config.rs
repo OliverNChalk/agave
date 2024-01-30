f50af36673ebeee6cd86a4cc174b06310b27a08c
Brennan <brennan.watt@solana.com>
2024-01-30 10:49:43 -0800

    Fix up debug messages (#264)
    fix up debug message

--- adversary/src/block_generator_config.rs
+++ adversary/src/block_generator_config.rs
@@ -30,13 +30,16 @@ impl From<BlockGeneratorAccountsSource> for Arc<AccountsFile> {
     fn from(block_generator_config: BlockGeneratorAccountsSource) -> Arc<AccountsFile> {
         match block_generator_config {
             BlockGeneratorAccountsSource::AccountsPath(file_name) => {
-                let file_content = std::fs::read_to_string(file_name)
-                    .expect("Failed to read the accounts file.\nPath: {file_name}");
+                let file_content = std::fs::read_to_string(&file_name).unwrap_or_else(|err| {
+                    panic!("Failed to read the accounts file.\nPath: {file_name:?}\nError: {err}")
+                });
                 let accounts = serde_json::from_str::<AccountsFileRaw>(&file_content)
-                    .expect(
-                        "Failed to parse accounts file.\nPath: \
-                         {file_name}\nContent:\n{file_content}",
-                    )
+                    .unwrap_or_else(|err| {
+                        panic!(
+                            "Failed to parse accounts file.\nPath: {file_name:?}\nError: \
+                             {err}\nContent:\n{file_content}"
+                        )
+                    })
                     .into();
                 Arc::new(accounts)
             }
