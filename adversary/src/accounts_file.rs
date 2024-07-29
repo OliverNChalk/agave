e1b91f78f0d1f2f2415d3098a0472bae8c671b0d
kirill lykov <kirill.lykov@anza.xyz>
2024-07-29 18:43:51 +0200

    Add unit test and comments to `AccountsFile` (#378)

--- adversary/src/accounts_file.rs
+++ adversary/src/accounts_file.rs
@@ -1,10 +1,16 @@
 use {serde::Deserialize, solana_keypair::Keypair, solana_pubkey::Pubkey, std::str::FromStr};
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, PartialEq)]
 pub struct AccountsFile {
+    /// Account owner for max_size and sized_accounts.
+    /// If specified, program can modify these accounts.
     pub owner_program_id: Option<Pubkey>,
+    /// Accounts used to pay for transactions.
+    /// Many are used to avoid introducing dependencies between transactions.
     pub payers: Vec<Keypair>,
+    /// Accounts of size `MAX_PERMITTED_DATA_LENGTH`
     pub max_size: Vec<Keypair>,
+    /// Pubkeys of programs used for program cache attacks.
     pub program_ids_jit_attack: Vec<Pubkey>,
 }
 
@@ -15,20 +21,8 @@ impl AccountsFile {
         max_size: Option<&[Keypair]>,
         program_ids_jit_attack: Option<&[Pubkey]>,
     ) -> Self {
-        let payers: Vec<Keypair> = match payers {
-            Some(payers) => payers
-                .iter()
-                .map(|keypair| keypair.insecure_clone())
-                .collect(),
-            None => Vec::new(),
-        };
-        let max_size: Vec<Keypair> = match max_size {
-            Some(max_size) => max_size
-                .iter()
-                .map(|keypair| keypair.insecure_clone())
-                .collect(),
-            None => Vec::new(),
-        };
+        let payers: Vec<Keypair> = clone_accounts_or_empty(payers);
+        let max_size: Vec<Keypair> = clone_accounts_or_empty(max_size);
         let program_ids_jit_attack: Vec<Pubkey> = match program_ids_jit_attack {
             Some(program_ids_jit_attack) => program_ids_jit_attack.to_vec(),
             None => Vec::new(),
@@ -61,15 +55,22 @@ impl Clone for AccountsFile {
     }
 }
 
+fn clone_accounts_or_empty(accounts: Option<&[Keypair]>) -> Vec<Keypair> {
+    match accounts {
+        Some(accounts) => accounts.iter().map(Keypair::insecure_clone).collect(),
+        None => Vec::new(),
+    }
+}
+
 impl From<AccountsFileRaw> for AccountsFile {
-    fn from(raw: AccountsFileRaw) -> Self {
-        let AccountsFileRaw {
+    fn from(
+        AccountsFileRaw {
             owner_program_id,
             payers,
             max_size,
             program_ids,
-        } = raw;
-
+        }: AccountsFileRaw,
+    ) -> Self {
         let payers = payers.into_iter().map(Into::into).collect();
         let max_size = max_size.into_iter().map(Into::into).collect();
 
@@ -111,5 +112,50 @@ impl From<KeypairRaw> for Keypair {
     fn from(raw: KeypairRaw) -> Self {
         assert_eq!(raw.secret_key.len(), 64);
         Self::new_from_array(raw.secret_key[..32].try_into().unwrap())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, solana_signer::Signer};
+
+    #[test]
+    fn test_deserialize_full_accounts_file() {
+        let keypair = Keypair::new();
+        let pubkey = keypair.pubkey().to_string();
+        let secretkey: Vec<u8> = keypair.to_bytes().to_vec();
+        let json_data = serde_json::json!({
+            "owner_program_id": pubkey,
+            "payers": [
+                {
+                    "publicKey": pubkey,
+                    "secretKey": secretkey,
+                },
+                {
+                    "publicKey": pubkey,
+                    "secretKey": secretkey,
+                },
+            ],
+            "max_size": [
+                {
+                    "publicKey": pubkey,
+                    "secretKey": secretkey,
+                },
+            ],
+            "program_ids": [pubkey, pubkey],
+        })
+        .to_string();
+
+        let accounts = serde_json::from_str::<AccountsFileRaw>(&json_data)
+            .expect("The test json should be properly formatted.");
+        assert_eq!(
+            AccountsFile::from(accounts),
+            AccountsFile {
+                owner_program_id: Some(keypair.pubkey()),
+                payers: vec![keypair.insecure_clone(), keypair.insecure_clone()],
+                max_size: vec![keypair.insecure_clone()],
+                program_ids_jit_attack: vec![keypair.pubkey(), keypair.pubkey()],
+            }
+        )
     }
 }
