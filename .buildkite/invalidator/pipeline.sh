d5142061adf9295cdcdd23454bda25dd2720d292
Yihau Chen <a122092487@gmail.com>
2024-09-23 14:13:56 +0800

    ci: return earlier when the noCI label is present (#437)
    * ci: return earlier when the noCI label is present
    
    * need a new commit to trigger pipeline if noCI is used

--- .buildkite/invalidator/pipeline.sh
+++ .buildkite/invalidator/pipeline.sh
@@ -5,6 +5,14 @@ set -e
 here=$(dirname "$0")
 
 if [[ "$BUILDKITE_PULL_REQUEST" != "false" ]]; then
+  labels=$(gh pr view "$BUILDKITE_PULL_REQUEST" --json labels --jq '.labels[].name')
+  for label in $labels; do
+    if [[ $label == "noCI" ]]; then
+      echo "noCI is present, skip CI"
+      exit 0
+    fi
+  done
+
   "$here"/_pipeline.sh
 else
   export TRIGGER_ALL_STEPS=1
