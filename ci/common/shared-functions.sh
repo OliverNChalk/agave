cf40749e71011d17e2fcd0fa41670368bab3c00c
Yihau Chen <a122092487@gmail.com>
2024-11-01 19:36:12 +0800

    ci: collect master-next test data (#464)
    * ci: collect master-next branch test results
    
    * feedback

--- ci/common/shared-functions.sh
+++ ci/common/shared-functions.sh
@@ -13,6 +13,21 @@ need_to_upload_test_result() {
     fi
   done
 
+  # Invalidator `master` branch is rebased regularly, and we want to collect
+  # stats for CI runs for those rebases.  The rebase process uses `master-next`
+  # as a candidate branch for the next `master` content.
+  #
+  # Candidates are created as normal PRs, though they are never merged, they
+  # are only used for CI runs.  So we are looking for pull requests for the
+  # `master` branch, with the source branch named `master-next`, coming from any
+  # fork.  "$CI_BRANCH" will have a value of "user-fork:master-next", so we
+  # pattern match.
+  if [[ -n "$CI_PULL_REQUEST" \
+      && "$CI_BASE_BRANCH" == "master" \
+      && "$CI_BRANCH" == *:master-next ]]; then
+    return 0
+  fi
+
   return 1
 }
 
