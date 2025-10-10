a01c25431d2bab5ff762d646e162f3ce3392c65e
Illia Bobyr <illia.bobyr@anza.xyz>
2025-10-09 20:27:24 -0700

    buildkite pipeline: Increase task parallelism
    Current BuildKite executions are quite inefficient, when enough agents
    are available.  See the commit content for the reasoning.

--- .buildkite/invalidator/_pipeline.sh
+++ .buildkite/invalidator/_pipeline.sh
@@ -155,6 +155,58 @@ wait_step() {
   echo "  - wait" >>pipeline
 }
 
+# On parallelism
+#
+# The following is the reasoning as to why `parallelism` parameter is set to
+# 8/16 for `stable-partition`/`local-cluster-partition` steps respectively.
+#
+# Having more steps that are smaller is generally better for scheduling.  And
+# better for retries.
+#
+# In terms of scheduling, as he have 9 agents, and the steps have rather varying
+# length, some agents before free much faster then the rest, and if they do not
+# have any work to do, we are just wasting an opportunity to do more work
+# faster.
+#
+# If a step fails, CI will retry it up to 3 times.  But we combine multiple
+# tests in the same CI step.  Meaning we are going to retry tests that have
+# actually succeeded.  Smaller steps mean that our retries waste less compute
+# and finish faster.
+#
+# When split into 2, `stable-partition` steps can run for about 15 minutes, but
+# can take up to 40 minutes due to internal retries - this is when steps
+# complete successfully still on the first attempt.
+# When when split into 6, `local-cluster-partition` steps can take between 8 and
+# 17 minutes.  But with retries, time can go up.
+#
+# With 2 splits for `stable-partition` and 6 splits for
+# `local-cluster-partition` we see that the first agent becomes vacant after
+# less than 9 minutes of the pipeline execution.  Yet, the pipeline still takes
+# 40 minutes due to the length of the longest task.  For the last 20 minutes,
+# only 3 agents have any work to do.  And for the last 12 minutes, only a single
+# agent is busy.
+#
+# There is, unfortunately, considerable overhead in scheduling and compilation
+# on each agent, for every step.  Seems like we do not share compilation results
+# from the previous invocations even on the same agent.
+#
+# `stable-partition` tests take 2 minutes to compile, before any execution can
+# start.  `local-cluster-partition` takes take 3 minutes to compile.  There are
+# also scheduling delays in the order of 1/2 minute, between two tasks on the
+# same agent.
+#
+# We need to consider this overhead.  And in practice, we see it increasing the
+# total pipeline execution time.  With 10 and 30 steps for `stable-partition`
+# and `local-cluster-partition` respectively, this overhead is already causing
+# the total time to approach 32 minutes.  While a lucky run for the 2/6 splits
+# can be just 25 minutes.
+#
+# 8/16 splits for `stable-partition`/`local-cluster-partition` respectively is
+# somewhere in the middle.  It seems like it is still brining the shortest
+# execution time up, to around 30 minutes (though I've seen a 22 minute run),
+# but it should reduce total time variance and greatly reduce the impact of
+# retries, compared to the 2/6 splits case.
+
 echo "steps:" >pipeline
 
 ## sanity
@@ -180,7 +232,7 @@ wait_step
 
 ## stable
 if affects .rs$ ^ci/rust-version.sh ^ci/test-stable.sh ; then
-  parallelism=2
+  parallelism=8
   for i in $(seq 1 $parallelism); do
     add_step_in_docker "stable-partition-$i" 60 \
       "ci/stable/run-partition.sh $i $parallelism" 3
@@ -200,7 +252,7 @@ fi
 if affects .rs$ ; then
 
   ## local-cluster
-  parallelism=6
+  parallelism=16
   for i in $(seq 1 $parallelism); do
     add_step_in_docker "local-cluster-partition-$i" 60 \
       "ci/stable/run-local-cluster-partially.sh $i $parallelism" 3
