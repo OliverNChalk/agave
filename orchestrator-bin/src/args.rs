08520dfbc83c1216f2e4598de3d492fcc93e0e60
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2026-04-08 16:03:30 -0500

    j

--- orchestrator-bin/src/args.rs
+++ orchestrator-bin/src/args.rs
@@ -3,7 +3,7 @@ use {clap::Parser, clap_v4 as clap, std::path::PathBuf};
 #[derive(Parser)]
 #[command(name = "agave-orchestrator")]
 pub(crate) struct Args {
-    /// File descriptor for the validator<>orchestrator UDS.
+    /// File descriptor for the validator <> orchestrator UDS.
     #[arg(long = "orch-fd")]
     pub(crate) orch_fd: i32,
 
